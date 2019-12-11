838145562d406de3c6c5888192891f274371b712
renzon <renzon@gmail.com>
2019-12-10 21:46:16 -0300

    testes com algoritmos determinísticos

--- tests/teste_de_excecao.py
+++ tests/teste_de_excecao.py
@@ -8,3 +8,18 @@ def divisao(numerador, denominador):
 def testar_excecao():
     with pytest.raises(ZeroDivisionError):
         divisao(2, 0)
+
+
+def soma(n, n2):
+    return n+n2
+
+
+@pytest.mark.parametrize(
+    'x', [1, 2, 3]
+)
+@pytest.mark.parametrize(
+    'y', [1, 2, 3]
+)
+def test_soma(x, y):
+    resultado = soma(x, y)
+    assert resultado - x == y
